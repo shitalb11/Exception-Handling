71b3fc37634d790179492718ee1634d1132e2396
Shital Bhosale <shitalb075@gmail.com>
2026-08-01 23:37:43 +0530

    neww

--- src/main/java/org/example/ExceptionHandling/Demo1.java
+++ src/main/java/org/example/ExceptionHandling/Demo1.java
@@ -1,4 +1,9 @@
 package org.example.ExceptionHandling;
 
 public class Demo1 {
+    public static void main(String[] args) {
+        int[] arr1 = {10,20,30,40,50};
+
+        int[] arr2 = new int[5];
+    }
 }
