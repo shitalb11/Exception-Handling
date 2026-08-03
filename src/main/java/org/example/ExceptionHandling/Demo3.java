d2285e4f9d0572f9ad0a5edb397a766b615eaee6
Shital Bhosale <shitalb075@gmail.com>
2026-08-03 19:44:06 +0530

    new

--- src/main/java/org/example/ExceptionHandling/Demo3.java
+++ src/main/java/org/example/ExceptionHandling/Demo3.java
@@ -1,7 +1,22 @@
 package org.example.ExceptionHandling;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Demo3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter The Two Number");
+        try{
+            int number1 = scanner.nextInt();
+            int number2 = scanner.nextInt();
+            System.out.println("Division is " + number1 / number2);
+            System.out.println("rest of the try");
+        }catch (ArithmeticException e){
+            System.out.println(e.getMessage());
+        }catch (InputMismatchException e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("rest of the code in main");
     }
 }
