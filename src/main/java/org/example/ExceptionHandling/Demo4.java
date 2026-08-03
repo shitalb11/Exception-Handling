f44d9f6dc37b2f868fd253a412bd219718040ba3
Shital Bhosale <shitalb075@gmail.com>
2026-08-03 19:49:07 +0530

    new

--- src/main/java/org/example/ExceptionHandling/Demo4.java
+++ src/main/java/org/example/ExceptionHandling/Demo4.java
@@ -1,4 +1,13 @@
 package org.example.ExceptionHandling;
 
 public class Demo4 {
+    public static void main(String[] args) {
+        try {
+            System.out.println(10 / 0);
+            System.out.println("Rest of the Try");
+        } catch (NullPointerException npe){
+            System.out.println("NPE");
+        }
+        System.out.println("rest of the main");
+    }
 }
