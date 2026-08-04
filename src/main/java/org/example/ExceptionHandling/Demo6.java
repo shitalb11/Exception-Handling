184a492d4752804bee25436f8898dbf7cf895e72
Shital Bhosale <shitalb075@gmail.com>
2026-08-04 18:03:31 +0530

    neW

--- src/main/java/org/example/ExceptionHandling/Demo6.java
+++ src/main/java/org/example/ExceptionHandling/Demo6.java
@@ -2,6 +2,13 @@ package org.example.ExceptionHandling;
 
 public class Demo6 {
     public static void main(String[] args) {
+        try{
+            try{
+                System.out.println(10 / 0);
+            } catch (ArithmeticException e){
+                System.out.println("inner catch");
+            }
+        }
 
     }
 }
