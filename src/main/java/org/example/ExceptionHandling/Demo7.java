4ed36a2d2a3b4b225d63eb9d069e30c5128860a6
Shital Bhosale <shitalb075@gmail.com>
2026-08-04 18:08:09 +0530

    new

--- src/main/java/org/example/ExceptionHandling/Demo7.java
+++ src/main/java/org/example/ExceptionHandling/Demo7.java
@@ -1,4 +1,12 @@
 package org.example.ExceptionHandling;
 
 public class Demo7 {
+    public static void main(String[] args) {
+        try{
+            System.out.println(10 / 0);
+        }finally {
+            System.out.println("finally block");
+        }
+        System.out.println("rest of the main");
+    }
 }
