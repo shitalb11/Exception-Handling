cbe2855bb34d8cbc60044cbf30076ce710a34abd
Shital Bhosale <shitalb075@gmail.com>
2026-08-09 16:33:44 +0530

    new

--- src/main/java/org/example/ExceptionHandling/Demo9.java
+++ src/main/java/org/example/ExceptionHandling/Demo9.java
@@ -1,5 +1,16 @@
 package org.example.ExceptionHandling;
 
 public class Demo9 {
-
+    public static void main(String[] args) {
+        System.out.println(getNumber());
+    }
+    private static int getNumber(){
+        try{
+            return 1;
+        }catch (Exception e){
+            return 2;
+        }finally {
+            return 3;
+        }
+    }
 }
