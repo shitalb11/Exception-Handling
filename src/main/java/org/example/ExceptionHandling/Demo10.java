67005660df07822cf87ef201c93c64eacc3d957b
Shital Bhosale <shitalb075@gmail.com>
2026-08-19 22:25:32 +0530

    new

--- src/main/java/org/example/ExceptionHandling/Demo10.java
+++ src/main/java/org/example/ExceptionHandling/Demo10.java
@@ -1,4 +1,19 @@
 package org.example.ExceptionHandling;
 
 public class Demo10 {
+    public static void main(String[] args) {
+    try {
+        String str = null;
+        System.out.println(str.length());
+        System.out.println(10 / 0);
+        int[] array = {};
+        System.out.println(array[0]);
+    } catch (ArithmeticException e) {
+        System.out.println("invalid denominator");
+    } catch (NullPointerException e) {
+        System.out.println("null pointer");
+    } catch (ArrayIndexOutOfBoundsException ae) {
+        System.out.println("invalid index");
+    }
+  }
 }
