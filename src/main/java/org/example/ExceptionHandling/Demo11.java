09b49ccc24d585ccab1551ec1e3b1087badbf4be
Shital Bhosale <shitalb075@gmail.com>
2026-08-19 22:28:07 +0530

    new

--- src/main/java/org/example/ExceptionHandling/Demo11.java
+++ src/main/java/org/example/ExceptionHandling/Demo11.java
@@ -1,4 +1,14 @@
 package org.example.ExceptionHandling;
 
 public class Demo11 {
+    public static void main(String[] args) {
+        try {
+            String str = "null";
+            System.out.println(str.length());
+            System.out.println(10 / 2);
+            int[] array = {};
+            System.out.println(array[0]);
+        }
+    }
+
 }
