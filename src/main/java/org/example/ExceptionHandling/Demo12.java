2d62a41a22776a917afcd3f2cf2b3e044beddf49
Shital Bhosale <shitalb075@gmail.com>
2026-08-20 22:27:25 +0530

    new

--- src/main/java/org/example/ExceptionHandling/Demo12.java
+++ src/main/java/org/example/ExceptionHandling/Demo12.java
@@ -8,6 +8,12 @@ public class Demo12 {
             System.out.println(10 / 0);
             int[] array = {};
             System.out.println(array[0]);
+        }catch (ArithmeticException e) {
+            System.out.println("invalid denominator");
+        } catch (NullPointerException e) {
+            System.out.println("null pointer");
+        } catch (ArrayIndexOutOfBoundsException ae) {
+            System.out.println("invalid index");
         }
     }
 }
