befd353e511b743c21386f808e8de19fe613ecdd
Shital Bhosale <shitalb075@gmail.com>
2026-08-21 20:54:58 +0530

    new

--- src/main/java/org/example/ExceptionHandling/Demo13.java
+++ src/main/java/org/example/ExceptionHandling/Demo13.java
@@ -1,4 +1,15 @@
 package org.example.ExceptionHandling;
 
 public class Demo13 {
+    public static void main(String[] args) {
+        try {
+            System.out.println(divide(100, 2));
+        } catch (ArithmeticException e) {
+            System.out.println("invalid denominator");
+        }
+        System.out.println("rest of the main");
+    }
+    private static int divide(int number1, int number2) throws ArithmeticException {
+        return number1 / number2;
+    }
 }
