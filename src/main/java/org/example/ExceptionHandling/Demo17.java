2b67811fe273d8e76876209dc901271acafbd7b5
Shital Bhosale <shitalb075@gmail.com>
2026-08-24 23:17:21 +0530

    new

--- src/main/java/org/example/ExceptionHandling/Demo17.java
+++ src/main/java/org/example/ExceptionHandling/Demo17.java
@@ -1,4 +1,8 @@
 package org.example.ExceptionHandling;
 
 public class Demo17 {
+    public static void main(String[] args) {
+        //System.out.println(10/0);
+        throw new ArithmeticException("/ by zero");
+    }
 }
