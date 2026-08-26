aa3f9b78741e36a2c03116ff88655b7921416a51
Shital Bhosale <shitalb075@gmail.com>
2026-08-26 23:01:58 +0530

    new

--- src/main/java/org/example/ExceptionHandling/Demo19.java
+++ src/main/java/org/example/ExceptionHandling/Demo19.java
@@ -1,7 +1,11 @@
 package org.example.ExceptionHandling;
 
+import java.util.Scanner;
+
 public class Demo19 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = scanner.nextInt();
     }
 }
