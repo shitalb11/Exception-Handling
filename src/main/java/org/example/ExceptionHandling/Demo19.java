ef72e61d9c6ef30c847746743b2e05dfabbbfeb7
Shital Bhosale <shitalb075@gmail.com>
2026-08-26 23:03:39 +0530

    new

--- src/main/java/org/example/ExceptionHandling/Demo19.java
+++ src/main/java/org/example/ExceptionHandling/Demo19.java
@@ -7,5 +7,14 @@ public class Demo19 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your age");
         int age = scanner.nextInt();
+        if (age < 18) {
+            try {
+                throw new InvalidAgeException("under 18");
+            } catch (InvalidAgeException e) {
+                System.out.println("Invalid age, please try after completing 18 again");
+            }
+        } else {
+            System.out.println("You are above 18 and eligible to vote");
+        }
     }
 }
