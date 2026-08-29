0cbb6d25b003badd36a10061e2680428673f8eb4
Shital Bhosale <shitalb075@gmail.com>
2026-08-29 23:19:15 +0530

    new

--- src/main/java/org/example/ExceptionHandling/InvalidAgeException.java
+++ src/main/java/org/example/ExceptionHandling/InvalidAgeException.java
@@ -1,4 +1,11 @@
 package org.example.ExceptionHandling;
 
-public class InvalidAgeException {
+public class InvalidAgeException extends Exception{
+    public InvalidAgeException(){
+
+}
+
+public InvalidAgeException(String s){
+    super(s);
+    }
 }
